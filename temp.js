8b4cfd2c84e82ddba083d37caeb02f71683e51bd
CMOL10 <79413136+CMOL10@users.noreply.github.com>
2021-12-13 13:58:20 +0000

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,34 @@
+
+google.charts.load('current', {
+  'packages': ['corechart', 'line'],
+});
+google.charts.setOnLoadCallback(initialize);
+
+function initialize() {
+  var queryString = encodeURIComponent('select B, C limit 400 label C "°C"');
+  var query = new google.visualization.Query(
+    'https://docs.google.com/spreadsheets/d/18_CfVYM0iMZPsUYZF9rINTBGo2ntZjdMA0OMoVq-NYs/gviz/tq?sheet=Sheet1&headers=0&tq=' + queryString);
+  query.send(handleSampleDataQueryResponse);
+}
+
+function handleSampleDataQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+
+  var options = {
+    height: 600,
+    hAxis: {
+      title: 'Hour',
+      direction: -1
+    },
+    vAxis: {
+      title: 'Temperature'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('graph'));
+  chart.draw(data, options);
+}
